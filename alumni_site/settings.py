86061c6d9956b792ff2450f81d5bf1009f890b38
Hiten Sethiya <hitensethiya@hotmail.com>
2017-10-11 10:34:48 +0530

    removed 'conn_max_age' arg

--- alumni_site/settings.py
+++ alumni_site/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 #ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
