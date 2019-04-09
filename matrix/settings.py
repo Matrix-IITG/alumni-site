11a733c1cf5a69a463982df62906901af228732f
sid573 <sinha170123048@iitg.ac.in>
2019-04-09 09:04:53 +0000

    dimension pany

--- matrix/settings.py
+++ matrix/settings.py
@@ -133,9 +133,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-AUTH_USER_MODEL = "home.User" 
+AUTH_USER_MODEL = "home.User"
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATIC_ROOT= os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = '/'
+
+
